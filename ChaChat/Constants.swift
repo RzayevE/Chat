84e76c56716ed03ffcf0756edd4ca87feafbd823
Elnur <eerzayev95@gmail.com>
2019-12-18 21:30:42 +0100

    first commit

---
+++ ChaChat/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  ChaChat
+//
+//  Created by Elnur Rzayev on 03/10/2018.
+//  Copyright © 2018 Elnur Rzayev. All rights reserved.
+//
+
+import Foundation
---
+++ ChaChat/LoginRegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginRegisterViewController.swift
+//  ChaChat
+//
+//  Created by Elnur Rzayev on 10/1/18.
+//  Copyright © 2018 Elnur Rzayev. All rights reserved.
+//
+
+import UIKit
+
+class LoginRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
